ba3209a69e52e769889deabe58ea5518319a48db
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-09-29 19:29:56 +0530

    commited

--- src/googleGeminiModelConfig.js
+++ src/googleGeminiModelConfig.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 
-const apiKey = process.env.GOOGLE_GEMINI_KEY;
+const apiKey = process.env.GOOGLE_GEMINI_API_KEY;
 
 
 const genAI = new GoogleGenerativeAI(apiKey);
