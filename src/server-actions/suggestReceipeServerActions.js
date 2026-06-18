aa7daa7687bcce2cb2ffc7835a2e5a49e3c3cc1e
Somenath Choudhury <81456073+somenath203@users.noreply.github.com>
2026-06-18 12:17:41 +0530

    Update LLM model to llama-3.1-8b-instant

--- src/server-actions/suggestReceipeServerActions.js
+++ src/server-actions/suggestReceipeServerActions.js
@@ -59,7 +59,7 @@ export const createNewReceipeSuggestion = async (prevState, formData) => {
 
 
         const llmModel = new ChatGroq({
-            model: "Llama3-8b-8192", 
+            model: "llama-3.1-8b-instant", 
             apiKey: process.env.GROQ_API_KEY,
         });
 
